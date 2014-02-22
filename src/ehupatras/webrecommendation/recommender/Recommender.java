58c7be7af39ec3a09b1080293f4cea6f4f7cc149
inigoperona <inigoperona@gmail.com>
2014-02-22 18:00:11 +0200

    A simple recommender class added. It needs more functionalities.

--- src/ehupatras/webrecommendation/recommender/Recommender.java
+++ src/ehupatras/webrecommendation/recommender/Recommender.java
@@ -1,21 +1,143 @@
 package ehupatras.webrecommendation.recommender;
 
+import ehupatras.suffixtree.stringarray.Edge;
+import ehupatras.suffixtree.stringarray.EdgeBag;
+import ehupatras.suffixtree.stringarray.Node;
 import ehupatras.suffixtree.stringarray.test.SuffixTreeStringArray;
 import java.util.*;
 
 public class Recommender {
 	private SuffixTreeStringArray m_gST;
+	private Node m_pointerNode = null;
+	private Edge m_pointerEdge = null;
+	private int m_pointerPositionInTheEdge = 0;
 	
 	public Recommender(SuffixTreeStringArray gST){
 		m_gST = gST;
+		m_pointerNode = m_gST.getRoot();
 	}
 	
-	public int[] getRecommendations(String[] sequenceuntilnow){
-		return null;
+	public void updatePointer(String newstep){
+		boolean isupdate = false;
+		
+		if(m_pointerNode==null && m_pointerEdge!=null){
+			ArrayList<String> label = m_pointerEdge.getLabel();
+			if(label.size()>m_pointerPositionInTheEdge){
+				String stepST = label.get(m_pointerPositionInTheEdge);
+				if(stepST.equals(newstep)){
+					//m_pointerNode = null;
+					//m_pointerEdge = m_pointerEdge;
+					m_pointerPositionInTheEdge++;
+					isupdate = true;
+				}
+			} else {
+				m_pointerNode = m_pointerEdge.getDest();
+				m_pointerEdge = null;
+				m_pointerPositionInTheEdge = 0;
+			}
+		}
+		
+		if(m_pointerEdge==null && m_pointerNode!=null){
+			// the possible edges
+			EdgeBag edges = m_pointerNode.getEdges();
+			ArrayList<Edge> edgesA = edges.values();
+			
+			// find the correct edge
+			// and initialize it in the first position
+			for(int i=0; i<edgesA.size(); i++){ // for each edge
+				Edge e = edgesA.get(i);
+				ArrayList<String> label = e.getLabel();
+				String stepST = label.get(m_pointerPositionInTheEdge);
+				if(stepST.equals(newstep)){
+					m_pointerNode = null;
+					m_pointerEdge = e;
+					m_pointerPositionInTheEdge++;
+					isupdate = true;
+					break;
+				}
+			}
+		}
+		
+		// it was impossible to move the pointer
+		if(!isupdate){
+			updatefailure();
+		}
 	}
 	
-	public int[] getRecommendations(ArrayList<String> sequenceuntilnow){
-		//m_gST.
-		return null;
+	public void updatefailure(){
+		// go to the root
+		m_pointerNode = m_gST.getRoot();
+		m_pointerEdge = null;
+		m_pointerPositionInTheEdge = 0;
 	}
+	
+	public ArrayList<String> getNextpossibleSteps(){
+		ArrayList<String> listOfURLs = new ArrayList<String>();
+		
+		// control if we are in the root or not
+		Node destnode = null;
+		ArrayList<String> label = null;
+		if(m_pointerEdge!=null){
+			label = m_pointerEdge.getLabel();
+			destnode = m_pointerEdge.getDest();
+		} else {
+			destnode = m_pointerNode;
+		}
+		
+		// take the possible steps
+		if(m_pointerEdge!=null && label.size()>m_pointerPositionInTheEdge){
+			listOfURLs.add(label.get(m_pointerPositionInTheEdge));
+		} else {
+			// take the first element of each edge label
+			EdgeBag edges = destnode.getEdges();
+			ArrayList<Edge> edgesA = edges.values();
+			for(int i=0; i<edgesA.size(); i++){ // for each edge
+				Edge e = edgesA.get(i);
+				ArrayList<String> label2 = e.getLabel();
+				listOfURLs.add(label2.get(0));
+			}
+		}
+		return listOfURLs;
+	}
+	
+	public static void main(String[] args){
+		// create the suffix tree
+		SuffixTreeStringArray st = new SuffixTreeStringArray();
+        String[] word1 = {"c", "a", "c", "a", "o"};
+        String[] word2 = {"b", "a", "n", "a", "n", "a"};
+        String[] word3 = {"m", "i", "l", "o"};
+        String[] word4 = {"c", "a", "r"};
+        st.putSequence(word1, 0);
+        st.putSequence(word2, 1);
+        st.putSequence(word3, 2);
+        st.putSequence(word4, 3);
+        st.printSuffixTree();
+        
+        // recommendations
+        Recommender rec = new Recommender(st);
+        
+        ArrayList<String> list;
+        
+        list = rec.getNextpossibleSteps();
+        
+        // recommendations from the root
+        System.out.println("---");
+        for(int i=0; i<list.size(); i++){ System.out.println(list.get(i)); }
+        
+        // paths that are exists
+        rec.updatePointer("c");
+        rec.updatePointer("a");
+        list = rec.getNextpossibleSteps();
+        rec.updatePointer("c");
+        list = rec.getNextpossibleSteps();
+        System.out.println("---");
+        for(int i=0; i<list.size(); i++){ System.out.println(list.get(i)); }
+	
+        // paths that they not exists
+        rec.updatePointer("z");
+        list = rec.getNextpossibleSteps();
+        System.out.println("---");
+        for(int i=0; i<list.size(); i++){ System.out.println(list.get(i)); }
+	}
+	
 }
