0949dc242abeb47cdb1abae162a671ddbed224b4
inigoperona <inigoperona@gmail.com>
2014-05-09 12:00:14 +0300

    Frequent Mining, MySPADE adapted to extract frequent sequences too.

--- src/ehupatras/sequentialpatternmining/MySPADE.java
+++ src/ehupatras/sequentialpatternmining/MySPADE.java
@@ -6,14 +6,21 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class MySPADE {
+	// sequences
+	private ArrayList<String[]> m_sequences;
 	private int m_nSeqs;
+	
+	// frequency of URLs
 	private double m_minsupport = 0.5d;
 	private String[] m_urls;
 	private int[] m_freqs;
+	private int m_minfreq;
 	
 	public MySPADE(ArrayList<String[]> sequences, float minsup){
-		m_minsupport = minsup;
+		m_sequences = sequences;
 		m_nSeqs = sequences.size();
+		m_minsupport = minsup;
+		m_minfreq = (int)Math.ceil( (float)m_nSeqs*m_minsupport );
 		
 		Hashtable<String,Integer> frequencies = new Hashtable<String,Integer>();
 		for(int i=0; i<sequences.size(); i++){
@@ -77,13 +84,11 @@ public class MySPADE {
 	
 	
 	public Object[] getFrequentSequencesLength1(){
-		int minfreq = (int)Math.ceil( (float)m_nSeqs*m_minsupport );
-		
-    	ArrayList<Integer> sups = new ArrayList<Integer>();
     	ArrayList<String> urls = new ArrayList<String>();
+    	ArrayList<Integer> sups = new ArrayList<Integer>();
 		for(int i=0; i<m_freqs.length; i++){
 			int freq = m_freqs[i];
-			if(freq>=minfreq){
+			if(freq>=m_minfreq){
 				String url = m_urls[i];
 				sups.add(freq);
 				urls.add(url);
@@ -97,6 +102,167 @@ public class MySPADE {
     	objA[0] = urls;
     	objA[1] = sups;
 		return objA;
+	}
+	
+	
+	public Object[] getFrequentSequencess(){
+		// get frequent URLs
+		Object[] objA = this.getFrequentSequencesLength1();
+		ArrayList<String> urls = (ArrayList<String>)objA[0];
+    	ArrayList<Integer> urlSups = (ArrayList<Integer>)objA[1];
+    	
+    	
+    	// remove those no frequent URLs
+    	ArrayList<ArrayList<String>> seqstrim = new ArrayList<ArrayList<String>>(); 
+    	for(int i=0; i<m_nSeqs; i++){
+    		
+    		// for each sequence
+    		String[] seq1 = m_sequences.get(i);
+    		ArrayList<String> seq2 = new ArrayList<String>(); 
+    		for(int j=0; j<seq1.length; j++){
+    			
+    			// for each URL, is frequent?
+    			String str1 = seq1[j];
+    			int ind = urls.indexOf(str1);
+    			if(ind!=-1){
+    				seq2.add(str1);
+    			}
+    		}
+    		
+    		// add the trimmed sequence
+    		if(seq2.size()>0){
+    			seqstrim.add(seq2);
+    		}
+    	}
+    	System.out.println("  myspade: initial iteration: " + 
+    				seqstrim.size() + "/" + m_nSeqs);
+    	
+ 
+    	// add URLs as a length-1 sequences
+    	ArrayList<String[]> seqs = new ArrayList<String[]>();
+    	ArrayList<Integer> seqSups = new ArrayList<Integer>();
+    	ArrayList<int[]> indsAL = new ArrayList<int[]>();
+    	for(int i=0; i<urls.size(); i++){
+    		String url = urls.get(i);
+    		String[] seq = new String[1];
+    		seq[0] = url;
+    		seqs.add(seq);
+    		seqSups.add(urlSups.get(i));
+    		int[] inds = new int[seqstrim.size()];
+    		for(int j=0; j<seqstrim.size(); j++){
+    			int ind = seqstrim.get(j).indexOf(url);
+    			inds[j] = ind;
+    		}
+    		indsAL.add(inds);
+    	}
+    	
+    	// create new sequences using frequent URLs
+    	for(int i=0; i<seqs.size(); i++){
+    		String[] seq = seqs.get(i);
+    		int[] inds = indsAL.get(i);
+    		
+    		// add frequent URL and count
+    		for(int j=0; j<urls.size(); j++){
+    			String url = urls.get(j);
+    			
+    			// see in how many sequences the next URL exists
+    			int freq = 0;
+    			int[] inds2 = new int[seqstrim.size()];
+        		for(int k=0; k<seqstrim.size(); k++){
+        			ArrayList<String> seqtr = seqstrim.get(k);
+        			int from = inds[k];
+        			int find = -1;
+        			if(from!=-1){
+        				find = this.indexOf(url, from+1, seqtr);
+        			}
+        			inds2[k] = find;
+        			if(find!=-1){
+        				freq++;
+        			}
+        		}
+        		
+        		// check the support
+    			if(freq>=m_minfreq){
+    				String[] newseq = new String[seq.length+1];
+    				int k;
+    				for(k=0; k<seq.length; k++){
+    					newseq[k] = seq[k];
+    				}
+    				newseq[k] = url;
+    				seqs.add(newseq);
+    				seqSups.add(freq);
+    				indsAL.add(inds2);
+    				System.out.println("  myspade: " + i + "x" + j + " iteration: " +
+    							freq + "/" + m_nSeqs);
+    			}
+    		}
+    	}
+    	
+    	// prepare the sequences to return
+    	Object[] objRe = new Object[2];
+    	objRe[0] = seqs;
+    	objRe[1] = seqSups;
+    	return objRe;
+	}
+	
+	private int indexOf(String url, int from, ArrayList<String> seq){
+		// find the requested url
+		int ind = -1;
+		for(int i=from; i<seq.size(); i++){
+			String url2 = seq.get(i);
+			if(url.equals(url2)){
+				ind = i;
+				break;
+			}
+		}
+		return ind;
+	}
+	
+	private String sequenceToString(String[] sequence){
+		String str;
+		if(sequence.length>0){
+			str = sequence[0];
+		} else {
+			str = "";
+		}
+		for(int j=1; j<sequence.length; j++){
+			str = str + "," + sequence[j];
+		}
+		return str;
+	}
+	
+	public static void main(String[] args){
+		String[] seq1 = new String[]{"1","2","3","4","5"};
+		String[] seq2 = new String[]{"1","2","3","9","8"};
+		String[] seq3 = new String[]{"1","2","7","6","9"};
+		String[] seq4 = new String[]{"1","6","8","7","5"};
+		ArrayList<String[]> seqs = new ArrayList<String[]>();
+		seqs.add(seq1);
+		seqs.add(seq2);
+		seqs.add(seq3);
+		seqs.add(seq4);
+		MySPADE msp = new MySPADE(seqs, 0.5f);
+		
+		// Lenth 1 sequences
+		System.out.println("--- Lenth 1 frequent sequences ---");
+		Object[] objA1 = msp.getFrequentSequencesLength1();
+		ArrayList<String> urls = (ArrayList<String>)objA1[0];
+		ArrayList<Integer> sups = (ArrayList<Integer>)objA1[1];
+		for(int i=0; i<urls.size(); i++){
+			System.out.println(urls.get(i) + " - " + sups.get(i));
+		}
+		
+		// frequent sequences
+		System.out.println("--- Frequent Sequences ---");
+		Object[] objA2 = msp.getFrequentSequencess();
+		ArrayList<String[]> freqSeqs = (ArrayList<String[]>)objA2[0];
+		ArrayList<Integer> freqSups = (ArrayList<Integer>)objA2[1];
+		for(int i=0; i<freqSeqs.size(); i++){
+			String[] freqseq = freqSeqs.get(i);
+			System.out.println("i" + i  + ": " + 
+						msp.sequenceToString(freqseq) + " - " + 
+						freqSups.get(i));
+		}
 	}
 	
 }
