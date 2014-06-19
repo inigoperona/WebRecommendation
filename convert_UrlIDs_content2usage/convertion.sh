bfc9a3166d339b94d6c8898e94d52157f5d2023e
inigoperona <inigoperona@gmail.com>
2014-06-19 17:36:37 +0200

    Tidy up the folder convert_UrlIDs_content2usage

--- convert_UrlIDs_content2usage/convertion.sh
+++ convert_UrlIDs_content2usage/convertion.sh
@@ -5,8 +5,8 @@
 while read line; do
   usageID=`echo $line | awk 'BEGIN{FS=","}{print $1}'`
   conteID=`echo $line | awk 'BEGIN{FS=","}{print $2}'`
-  distline=`grep "^$conteID," topicTestuakBatera-document-topic-distributuionsTestuhutsa.csv`
+  distline=`grep "^$conteID," topicTestuakBatera-document-topic-distributuionsTestuhutsa_contentID.csv`
   distline2=`echo $distline | awk 'BEGIN{FS=","}{print $2; for(i=3;i<=NF;i++){print ","$i}}'`
   echo $usageID","$distline2
-done < usa2cont.csv > document-topic-distributuions1_usageIDhutsa.csv
+done < usa2cont.csv > topicTestuakBatera-document-topic-distributuionsTestuhutsa_usageID_2.csv
 
