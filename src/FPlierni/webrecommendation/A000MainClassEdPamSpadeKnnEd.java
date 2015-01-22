06ac357e6f01b61ff229ee6f03958d8e0ccf01a7
lsestorain001 <lsestorain001@gmail.com>
2015-01-22 10:13:10 +0100

    Testing PAM k=100

--- src/FPlierni/webrecommendation/A000MainClassEdPamSpadeKnnEd.java
+++ src/FPlierni/webrecommendation/A000MainClassEdPamSpadeKnnEd.java
@@ -45,7 +45,7 @@ public class A000MainClassEdPamSpadeKnnEd {
 		String var_usage2contentFile = var_preprocessingWD + "/Content/usa2cont.csv";
 		String var_evalFile = "/evaluation.txt";
 		// system's parameters
-		int[] var_ks = {150}; // number of clusters
+		int[] var_ks = {100}; // number of clusters
 		float[] var_seqweights = {0.20f}; // Sequence Mining algorithm's minimum support
 		// metrics' parameters
 		int var_modePrRe = 0; // 0: strict - 1: relax, precision and recall computation
