bc6b27125940c40411b7d3fa62dabe03dcdc4145
inigoperona <inigoperona@gmail.com>
2015-08-31 14:39:56 +0200

    Cross-val 100 folds. 7 train, 2 val, 1 test.

---
+++ src/gipuzkoa_eus/webrecommendation/A021MainClassCrossValidation.java
@@ -0,0 +1,24 @@
+package gipuzkoa_eus.webrecommendation;
+
+import angelu.webrecommendation.A0000ParameterControl_angelu;
+
+public class A021MainClassCrossValidation {
+
+	public static void main(String[] args) {
+		
+		A0000ParameterControl_angelu param = new A0000ParameterControl_angelu(args);
+		
+		// take the start time of the program
+		long starttimeprogram = System.currentTimeMillis();
+
+		// RUN
+		param.loadDatabase();
+		param.loadDM();
+		param.createCrossValidation(7,2,1,100); // we have 83000 cases
+
+		// ending the program
+		long endtimeprogram = System.currentTimeMillis();
+		System.out.println("The program has needed " + (endtimeprogram-starttimeprogram)/1000 + " seconds.");
+	}
+	
+}
