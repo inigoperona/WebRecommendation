616592b9dff6ca15d9a5682a653f80fcd2909a1c
ainho <nhia83@hotmail.com>
2016-05-05 17:33:25 +0200

    more urls

--- src/gipuzkoa_eus/webrecommendation/A000MainClassPreprocess_10min.java
+++ src/gipuzkoa_eus/webrecommendation/A000MainClassPreprocess_10min.java
@@ -239,8 +239,8 @@ public class A000MainClassPreprocess_10min {
 		//String logfilesIndex = "/loglist2.txt";
 
 		//String basedirectory = "/home/ainhoa/workspace_WebRecommendation";
-		//String basedirectory = "/home/disk/AINHOA/EHU_LOGAK_13_04_2016/experiment1/00_preprocess";
-		String basedirectory = "20160400_experiments_EHU_LOG_LAGINA2/00_preprocess";
+		String basedirectory = "/home/disk/AINHOA/EHU_LOGAK_13_04_2016/experiment1/00_preprocess";
+		//String basedirectory = "20160400_experiments_EHU_LOG_LAGINA2/00_preprocess";
 		String logfilesIndex = "/loglist.txt";
 		
 		basedirectory = args[0];
